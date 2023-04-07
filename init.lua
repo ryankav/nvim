c795740b8b3b4de39dbafdf3306c6c931dd34a11
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-04-07 10:11:02 +0100

    adding plugin manager

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require "ryan.options" -- import ryan vimoptions
-require "ryan.keymaps" -- import ryan vimoptions
+require "ryan.keymaps" -- import ryan keymappings 
+require "ryan.plugins" -- import plugins 
 
