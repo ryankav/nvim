2c8f47debb727fae8bcf866551124b28dd235c84
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-04-26 09:27:21 +0100

    Setting up keymap directory and first setup for cmp

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -4,8 +4,13 @@ local M = {
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lsp"
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help"
   },
+  config = function()
+    local cmp = require("cmp")
+    mapping = require("lua.core.keymaps.plugins.cmp").mappings(cmp)
+  end
 }
 
 return M
