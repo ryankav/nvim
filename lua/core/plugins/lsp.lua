a4fdeaee6d489227c95cc79b43394364a805a5f3
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-05-04 20:19:29 +0100

    Changing diagnostic lsp

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -8,6 +8,29 @@ local M = {
     "hrsh7th/cmp-nvim-lsp-signature-help",
   },
   config = function()
+    local signs = {
+      { name = "DiagnosticSignError", text = "" },
+      { name = "DiagnosticSignWarn", text = "" },
+      { name = "DiagnosticSignHint", text = "" },
+      { name = "DiagnosticSignInfo", text = "" },
+    }
+
+    for _, sign in ipairs(signs) do
+      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
+
+    local diagnostic_config = {
+      virtual_text = true,
+      signs = {
+        active = signs,
+      },
+      update_in_insert = true,
+      underline = true,
+    }
+
+    vim.diagnostic.config(diagnostic_config)
+
+    -- setup the actual language servers
     require("core.lsp").setup()
   end,
 }
