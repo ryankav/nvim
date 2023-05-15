844c8c96d4ca7cf5ff8d603ba2c4c85364e71bcf
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-05-15 22:24:33 +0100

    adding on attach to clangd

--- lua/core/lsp/c.lua
+++ lua/core/lsp/c.lua
@@ -1,7 +1,13 @@
 local M = {
   servers = { "clangd" },
   setup = function()
-    require'lspconfig'.clangd.setup{}
+    local on_attach = function(_, buf)
+      require('core.keymaps.plugins.lsp').on_attach(buf)
+    end
+
+    require'lspconfig'.clangd.setup {
+      on_attach = on_attach
+    }
   end,
   null_ls_sources = {}
 }
