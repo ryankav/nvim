d41bb6d5b12b86f978de8600b8f557bb3b9e2e7f
Ryan Kavanagh <ryan.p.kav@gmail.com>
2024-04-24 18:08:56 +0100

    Adding the lsp buffer keybindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,5 +48,21 @@ return {
       server_opts.capailities = capabilities
       lspconfig[server].setup(server_opts)
     end
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+
+        map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+        map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+      end,
+    })
   end,
 }
